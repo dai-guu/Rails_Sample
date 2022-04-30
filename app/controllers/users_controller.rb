3b2e21bcd862bec2cb933e2ad21fcbcaf6beb8fc
Ubuntu <ubuntu@ip-172-31-17-84.ap-northeast-1.compute.internal>
2022-04-30 03:06:31 +0000

    [Add]micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def create
@@ -66,21 +66,13 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     
-  def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-  end
-  
-  def correct_user
+    def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
-  end
+    end
   
-  def admin_user
+    def admin_user
       redirect_to(root_url) unless current_user.admin?
-  end
+    end
   
 end
