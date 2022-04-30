53a94bff8e106828c662cc6b7613530e48f9398c
Ubuntu <ubuntu@ip-172-31-17-84.ap-northeast-1.compute.internal>
2022-04-30 11:51:53 +0000

    [Add]user follow

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,4 +13,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign up")
   end
+  
+  test "layout links when logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
 end
